dbd11d2639fa3c957c5c196b87f1b79b52817520
SateeshkumarGolusupudi <95600477+SateeshkumarGolusupudi@users.noreply.github.com>
2021-12-25 02:57:27 +0530

    Add files via upload

---
+++ If Marks.py
@@ -0,0 +1,34 @@
+a =int(input("Enter your marks "))
+if a>=90 and a<=100:
+    print ("Your Grade is A")
+elif a>=80 and a<90:
+    print ("Your Grade is B")
+elif a>=70 and a<80:
+    print ("Your Grade is C")
+elif a>=60 and a<70:
+    print ("Your Grade is D")
+elif a>=50 and a<60:
+    print ("Your Grade is E")
+else:
+    print ("You Are Failed")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
